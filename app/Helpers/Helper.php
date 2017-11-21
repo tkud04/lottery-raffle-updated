94c58a09c87ce9b1607c16a6822140d241f761b3
Rich Mama <postmaster.richmama@gmail.com>
2017-11-21 10:09:05 +0100

    Changed smtp to mailjet

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -25,7 +25,7 @@ class Helper implements HelperContract
                    if($type == "view")
                    {
                      Mail::send($view,$data,function($message) use($to,$subject){
-                           $message->from('dunphydavid83@gmail.com',"WorldLotteryUSA");
+                           $message->from('info@worldlotteryusa.com',"WorldLotteryUSA");
                            $message->to($to);
                            $message->subject($subject);
                           if(isset($data["has_attachments"]) && $data["has_attachments"] == "yes")
@@ -38,7 +38,7 @@ class Helper implements HelperContract
                    elseif($type == "raw")
                    {
                      Mail::raw($view,$data,function($message) use($to,$subject){
-                           $message->from('dunphydavid83@gmail.com',"WorldLotteryUSA");
+                           $message->from('info@worldlotteryusa.com',"WorldLotteryUSA");
                            $message->to($to);
                            $message->subject($subject);
                            if(isset($data["has_attachments"]) && $data["has_attachments"] == "yes")
