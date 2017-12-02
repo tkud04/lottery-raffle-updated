c79159324632ed0572c22403666f0fb3e48e9fa9
Rich Mama <postmaster.richmama@gmail.com>
2017-12-02 18:45:22 +0100

    Again

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -6,5 +6,6 @@ Interface HelperContract
         public function sendEmail($to,$subject,$data,$view,$type);
         public function createRaffle($data);
         public function getClients();
+        public function deleteClient($id);
 }
  ?>
